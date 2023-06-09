f76e7dcb419e8eb7fbc5b7ee8b03c1be54c52785
Caio Joca <52425267+hiatus@users.noreply.github.com>
2023-06-09 20:21:54 -0300

    Modularize project

---
+++ src/crypto.rs
@@ -0,0 +1,160 @@
+use std::fs;
+use std::io::{Read, Write, Error, ErrorKind};
+
+use rand::Rng;
+use libaes::Cipher;
+use litcrypt::{use_litcrypt, lc};
+
+
+pub const SIZE_IV: usize = 16;
+pub const SIZE_KEY: usize = libaes::AES_128_KEY_LEN;
+pub const SIZE_BLOCK: usize = 32768;
+
+macro_rules! nukrypt_key_ext  { () => { lc!(".nukrypt") } }
+
+use_litcrypt!("nukrypt");
+
+
+pub fn gen_key() -> [u8; SIZE_KEY] {
+	rand::thread_rng().gen::<[u8; SIZE_KEY]>()
+}
+
+pub fn gen_iv() -> [u8; SIZE_IV] {
+	rand::thread_rng().gen::<[u8; SIZE_IV]>()
+}
+
+pub fn read_key(path: &str, key: &mut [u8; SIZE_KEY]) -> Result<bool, Error> {
+	let mut fin = fs::File::open(path)?;
+
+	if fin.read(key)? != SIZE_KEY {
+		return Err(Error::new(
+			ErrorKind::UnexpectedEof, lc!("Failed to read key from file")
+		));
+	}
+
+	Ok(true)
+}
+
+pub fn write_key(path: &str, key: &[u8; SIZE_KEY]) -> Result<bool, Error> {
+	let mut fout = fs::File::create(path)?;
+	fout.write(key)?;
+
+	Ok(true)
+}
+
+pub fn encrypt_file(path: &str, key: &[u8; SIZE_KEY]) -> Result<bool, Error> {
+	let path_out = format!("{}{}", path, nukrypt_key_ext!());
+
+	let mut fin = fs::File::open(path)?;
+	let mut fout = fs::File::create(&path_out)?;
+
+	let mut buffer = [0u8; SIZE_BLOCK];
+
+	let iv = gen_iv();
+	let cipher: Cipher = Cipher::new_128(key);
+
+	fout.write(&iv)?;
+
+	loop {
+		let n = fin.read(&mut buffer[..])?;
+
+		if n == 0 {
+			break;
+		}
+
+		let cipher_block = cipher.cbc_encrypt(&iv, &buffer[..n]);    
+    
+		if fout.write(&cipher_block)? == 0 {
+			break;
+		}
+	}
+    
+	fs::remove_file(path)?;
+	Ok(true)
+}
+
+pub fn decrypt_file(path: &str, key: &[u8; SIZE_KEY]) -> Result<bool, Error> {
+	let path_out = match path.strip_suffix(nukrypt_key_ext!().as_str()) {
+		Some(s) => s.to_owned(),
+		None => path.to_string()
+	};
+
+	let mut fin = fs::File::open(path)?;
+	let mut fout = fs::File::create(&path_out)?;
+
+	let mut iv: [u8; SIZE_IV] = [0u8; SIZE_IV];
+	let mut buffer = [0u8; SIZE_BLOCK];
+
+	let cipher: Cipher = Cipher::new_128(key);
+
+	fin.read(&mut iv[..])?;
+
+	loop {
+		let n = fin.read(&mut buffer[..])?;
+    
+		if n == 0 {
+			break;
+		}
+    
+		let cipher_block = cipher.cbc_decrypt(&iv, &buffer[..n]);
+    
+		if fout.write(&cipher_block)? == 0 {
+			break;
+		}
+	}
+    
+	fs::remove_file(path)?;
+	Ok(true)
+}
+    
+pub fn encrypt_dir(path: &str, key: &[u8; SIZE_KEY]) -> Result<usize, Error> {
+	let mut counter: usize = 0;
+
+	for e in fs::read_dir(path).unwrap() {
+		let entry = e.unwrap();
+		let filetype = entry.file_type().unwrap();
+
+		if filetype.is_file() {
+			println!("{} {}", lc!("[#] Encrypting:"), entry.path().display());
+
+			match encrypt_file(entry.path().to_str().unwrap(), &key) {
+				Ok(b)  => { if b { counter += 1 } },
+				Err(e) => { println!("{} {}", lc!("[-] Failed:"), e) }
+			}
+		}
+		else
+		if filetype.is_dir() {
+			counter += encrypt_dir(entry.path().to_str().unwrap(), key).unwrap();
+		}
+	}
+
+	Ok(counter)
+}
+
+pub fn decrypt_dir(path: &str, key: &[u8; SIZE_KEY]) -> Result<usize, Error> {
+	let mut counter: usize = 0;
+
+	for e in fs::read_dir(path).unwrap() {
+		let entry = e.unwrap();
+		let filetype = entry.file_type().unwrap();
+
+		if filetype.is_file() {
+			if ! entry.file_name().to_str().unwrap().ends_with(nukrypt_key_ext!().as_str()) {
+				continue;
+			}
+
+			println!("{} {}", lc!("[#] Decrypting:"), entry.path().display());
+
+			match decrypt_file(entry.path().to_str().unwrap(), &key) {
+				Ok(b)  => { if b { counter += 1 } },
+				Err(e) => { println!("{} {}", lc!("[-] Failed:"), e) }
+			}
+		}
+		else
+		if filetype.is_dir() {
+			counter += decrypt_dir(entry.path().to_str().unwrap(), key).unwrap();
+		}
+	}
+
+	Ok(counter)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use std::{env, fs};
-use std::fs::File;
+mod crypto;
+
+use std::env;
 use std::path::Path;
 use std::time::Instant;
-use std::io::{Read, Write, Error, ErrorKind};
 
-use rand::{Rng};
-use libaes::Cipher;
 use litcrypt::{use_litcrypt, lc};
 
 
-const BLOCK_SIZE: usize = 64 * 1024;
-
-macro_rules! nukrypt_key { () => { lc!("nukrypt.key") } }
-macro_rules! nukrypt_extension { () => { lc!(".nukrypt") } }
+macro_rules! nukrypt_key_path { () => { lc!("nukrypt.key") } }
 macro_rules! nukrypt_banner { () => { lc!("Usage: nukrypt [encrypt|decrypt] [dir]..") } }
 
 use_litcrypt!("nukrypt");
@@ -21,7 +16,7 @@ use_litcrypt!("nukrypt");
 fn main() {
     let key_exists: bool;
     let mut decrypt: bool = false;
-    let mut key : [u8; 16] = [0u8; 16];
+    let mut key = [0u8; crypto::SIZE_KEY];
 
     let args: Vec<_> = env::args().collect();
 
@@ -30,14 +25,14 @@ fn main() {
         return;
     }
 
-    match read_key(nukrypt_key!().as_str(), &mut key) {
+    match crypto::read_key(nukrypt_key_path!().as_str(), &mut key) {
         Ok(_)  => { key_exists = true },
         Err(_) => { key_exists = false }
     }
 
     if args[1].eq(lc!("decrypt").as_str()) {
         if ! key_exists {
-            println!("{} {}", lc!("[!] Failed to read encryption key from"), nukrypt_key!());
+            println!("{} {}", lc!("[!] Failed to read encryption key from"), nukrypt_key_path!());
             return;
         }
 
@@ -46,15 +41,15 @@ fn main() {
     else
     if args[1].eq(lc!("encrypt").as_str()) {
         if key_exists {
-            println!("{} {}\n", lc!("[+] Encryption key read from"), nukrypt_key!());
+            println!("{} {}\n", lc!("[+] Encryption key read from"), nukrypt_key_path!());
         }
         else {
-            key = rand::thread_rng().gen::<[u8; 16]>();
+            key = crypto::gen_key();
 
-            match write_key(nukrypt_key!().as_str(), &key) {
-                Ok(_)  => println!("{} {}\n", lc!("[+] Encryption key saved to"), nukrypt_key!()),
+            match crypto::write_key(nukrypt_key_path!().as_str(), &key) {
+                Ok(_)  => println!("{} {}\n", lc!("[+] Encryption key saved to"), nukrypt_key_path!()),
                 Err(_) => {
-                    println!("{} {}", lc!("[!] Failed to write encryption key to"), nukrypt_key!());
+                    println!("{} {}", lc!("[!] Failed to write encryption key to"), nukrypt_key_path!());
                     return;
                 }
             }
@@ -82,10 +77,10 @@ fn main() {
         }
 
         if decrypt {
-            counter += decrypt_dir(&args[i], &key).unwrap();
+            counter += crypto::decrypt_dir(&args[i], &key).unwrap();
         }
         else {
-            counter += encrypt_dir(&args[i], &key).unwrap();
+            counter += crypto::encrypt_dir(&args[i], &key).unwrap();
         }
     }
 
@@ -101,145 +96,4 @@ fn main() {
             (before.elapsed().as_millis() as f32) / 1000.0, lc!("seconds")
         );
     }
-}
-
-fn read_key(path: &str, key: &mut [u8; 16]) -> Result<bool, Error> {
-    let mut fin = File::open(path)?;
-
-    if fin.read(key)? != 16 {
-        return Err(Error::new(
-            ErrorKind::UnexpectedEof,
-            lc!("Failed to read key from file")
-        ));
-    }
-
-    Ok(true)
-}
-
-fn write_key(path: &str, key: &[u8; 16]) -> Result<bool, Error> {
-    let mut fout = File::create(path)?;
-    fout.write(key)?;
-
-    Ok(true)
-}
-
-fn encrypt_file(path: &str, key: &[u8; 16]) -> Result<bool, Error> {
-    let path_out = format!("{}{}", path, nukrypt_extension!());
-
-    let mut fin = File::open(path)?;
-    let mut fout = File::create(&path_out)?;
-
-    let mut buffer = [0u8; BLOCK_SIZE];
-
-    let iv = rand::thread_rng().gen::<[u8; 16]>();
-    let cipher: Cipher = Cipher::new_128(key);
-
-    fout.write(&iv)?;
-
-    loop {
-        let n = fin.read(&mut buffer[..])?;
-
-        if n == 0 {
-            break;
-        }
-
-        let cipher_block = cipher.cbc_encrypt(&iv, &buffer[..n]);    
-
-        if fout.write(&cipher_block)? == 0 {
-            break;
-        }
-    }
-
-    fs::remove_file(path)?;
-    Ok(true)
-}
-
-fn decrypt_file(path: &str, key: &[u8; 16]) -> Result<bool, Error> {
-    let path_out = match path.strip_suffix(nukrypt_extension!().as_str()) {
-        Some(s) => s.to_owned(),
-        None => path.to_string()
-    };
-
-    let mut fin = File::open(path)?;
-    let mut fout = File::create(&path_out)?;
-
-    let mut iv: [u8; 16] = [0u8; 16];
-    let mut buffer = [0u8; BLOCK_SIZE];
-
-    let cipher: Cipher = Cipher::new_128(key);
-
-    fin.read(&mut iv[..])?;
-
-    loop {
-        let n = fin.read(&mut buffer[..])?;
-
-        if n == 0 {
-            break;
-        }
-
-        let cipher_block = cipher.cbc_decrypt(&iv, &buffer[..n]);
-
-        if fout.write(&cipher_block)? == 0 {
-            break;
-        }
-    }
-
-    fs::remove_file(path)?;
-    Ok(true)
-}
-
-fn encrypt_dir(path: &str, key: &[u8; 16]) -> Result<usize, Error> {
-    let mut counter: usize = 0;
-
-    for e in fs::read_dir(path).unwrap() {
-        let entry = e.unwrap();
-        let filetype = entry.file_type().unwrap();
-
-        if filetype.is_file() {
-            println!("{} {}", lc!("[#] Encrypting:"), entry.path().display());
-
-            match encrypt_file(entry.path().to_str().unwrap(), &key) {
-                Ok(b)  => { if b { counter += 1 } },
-                Err(e) => {
-                    println!("{} {}", lc!("[-] Failed:"), e)
-                }
-            }
-        }
-        else
-        if filetype.is_dir() {
-            counter += encrypt_dir(entry.path().to_str().unwrap(), key).unwrap();
-        }
-    }
-
-    Ok(counter)
-}
-
-fn decrypt_dir(path: &str, key: &[u8; 16]) -> Result<usize, Error> {
-    let mut counter: usize = 0;
-
-    for e in fs::read_dir(path).unwrap() {
-        let entry = e.unwrap();
-        let filetype = entry.file_type().unwrap();
-
-        if filetype.is_file() {
-            if ! entry.file_name().to_str().unwrap().ends_with(nukrypt_extension!().as_str()) {
-                continue;
-            }
-
-            println!("{} {}", lc!("[#] Decrypting:"), entry.path().display());
-    
-            match decrypt_file(entry.path().to_str().unwrap(), &key) {
-                Ok(b)  => { if b { counter += 1 } },
-                Err(e) => {
-                    println!("{} {}", lc!("[-] Failed:"), e)
-                }
-            }
-        }
-        else
-        if filetype.is_dir() {
-            counter += decrypt_dir(entry.path().to_str().unwrap(), key).unwrap();
-        }
-    }
-
-    Ok(counter)
 }
